b20ccb67c7a0d7eb9ad65d75fe74f35d99ed74a8
Francois Joubert <fjoubert@ska.ac.za>
2014-10-31 11:24:18 +0200

    added mockup control buttons for vds control

--- app/video/video.js
+++ app/video/video.js
@@ -3,6 +3,46 @@ angular.module('katGui.video', [])
 
     //ssh to the box, do
     //motion -n -c /var/kat/vds_motion.conf
-    .controller('VideoCtrl', function ($scope, $window) {
+    .controller('VideoCtrl', function ($scope) {
+
+        $scope.panLeft = function () {
+            console.log('pan left');
+        };
+
+        $scope.panFarLeft = function () {
+            console.log('pan far left');
+        };
+
+        $scope.panRight = function () {
+            console.log('pan right');
+        };
+
+        $scope.panFarRight = function () {
+            console.log('pan far right');
+        };
+
+        $scope.tiltUp = function () {
+            console.log('tilt up');
+        };
+
+        $scope.tiltFarUp = function () {
+            console.log('tilt far up');
+        };
+
+        $scope.tiltDown = function () {
+            console.log('tilt down');
+        };
+
+        $scope.tiltFarDown = function () {
+            console.log('tilt far down');
+        };
+
+        $scope.rememberPos = function () {
+            console.log('rememberPos');
+        };
+
+        $scope.cyclePos = function () {
+            console.log('cyclePos');
+        };
 
     });
