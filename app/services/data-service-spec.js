4690c5d920fa5e818b57458effa899b9887ea760
Francois Joubert <fjoubert@ska.ac.za>
2014-12-18 10:34:05 +0200

    removed deleted dependency in old data service

--- app/services/data-service-spec.js
+++ app/services/data-service-spec.js
@@ -2,7 +2,7 @@ describe('DataService', function() {
 
     beforeEach(module('katGui'));
 
-    it('should...', inject(function(AuthService) {
+    it('should...', inject(function() {
 
         //expect(AuthService.doSomething()).toEqual('something');
 
