888208a95f603423550777333d39825dc493dd5a
Francois Joubert <fjoubert@ska.ac.za>
2015-02-17 10:18:48 +0200

    reworked alarms listing with an improved look and feel

--- app/services/monitor-service.js
+++ app/services/monitor-service.js
@@ -129,7 +129,6 @@
             }
 
             messageObj.priority = alarmPriority;
-            messageObj.message = messageObj.value;
             messageObj.name = messageName.replace('kataware:alarm.', '');
             messageObj.dateUnix = messageObj.timestamp;
             messageObj.date = moment.utc(messageObj.timestamp, 'X').format('HH:mm:ss DD-MM-\'YY');
