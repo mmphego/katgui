928fd6e991f3e0a14c38e070b7bf9d95ab010c58
Francois Joubert <fjoubert@ska.ac.za>
2015-08-03 17:10:39 +0200

    calculate progress in overview tab too

--- app/scheduler/observations/observations-overview.js
+++ app/scheduler/observations/observations-overview.js
@@ -3,7 +3,7 @@
     angular.module('katGui.scheduler')
         .controller('SubArrayExecutorOverview', SubArrayExecutorOverview);
 
-    function SubArrayExecutorOverview($state, ObsSchedService) {
+    function SubArrayExecutorOverview($state, ObsSchedService, $interval) {
 
         var vm = this;
         vm.subarrays = ObsSchedService.subarrays;
@@ -12,6 +12,18 @@
         vm.navigateToSchedulerDetails = function (subarray_id) {
             $state.go('scheduler.observations.detail', {subarray_id: subarray_id});
         };
+
+        vm.progressInterval = $interval(function () {
+            ObsSchedService.scheduleData.forEach(function (sb) {
+                sb.progress = vm.sbProgress(sb);
+            });
+        }, 1500);
+
+        $scope.$on('$destroy', function () {
+            if (vm.progressInterval) {
+                $interval.cancel(vm.progressInterval);
+            }
+        });
     }
 
 })();
