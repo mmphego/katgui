9d435ee75879298e574cfc6227749756764d09bf
Francois Joubert <fjoubert@ska.ac.za>
2015-08-12 15:25:36 +0200

    split resources between nodes in sensor list

--- app/services/sensors-service.js
+++ app/services/sensors-service.js
@@ -3,7 +3,7 @@
     angular.module('katGui.services')
         .service('SensorsService', SensorsService);
 
-    function SensorsService($rootScope, SERVER_URL, KatGuiUtil, $timeout, $q, $interval, $log, $http) {
+    function SensorsService($rootScope, SERVER_URL, KatGuiUtil, $timeout, $q, $interval, $log, $http, ConfigService) {
 
         var urlBase = SERVER_URL + '/katmonitor';
         var api = {};
@@ -184,6 +184,13 @@
             $http.get(urlBase + '/resource')
                 .then(function (result) {
                     for (var i in result.data) {
+                        for (var node in ConfigService.systemConfig.nodes) {
+                            var processList = Object.keys(ConfigService.systemConfig[node]);
+                            if (processList.indexOf(result.data[i].name) > -1) {
+                                result.data[i].node = node;
+                                break;
+                            }
+                        }
                         api.resources[result.data[i].name] = result.data[i];
                     }
                     deferred.resolve(api.resources);
