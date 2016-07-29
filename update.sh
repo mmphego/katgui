57110606f3c7f7661ff1b90193976f5976a80fc3
Francois Joubert <fjoubert@ska.ac.za>
2016-07-29 10:11:30 +0200

    better filename

--- update.sh
+++ update.sh
@@ -18,7 +18,7 @@ echo "## Compressing dist/ folder ..."
 GIT_REF=`git rev-parse --short HEAD`
 VERSION=`python -m json.tool dist/version.txt | grep -Po '\d\.\d\.\d'`
 DATE=`date +"%Y-%m-%d"`
-TIME=`date +"%H:%M"`
-FILENAME="katgui-$VERSION-$DATET$TTIME-$GIT_REF.tar.gz"
+TIME=`date +"%H.%M"`
+FILENAME="katgui-$VERSION-$DATE"T"$TIME-$GIT_REF.tar.gz"
 tar -zcvf $FILENAME dist/
 echo "## KATGUI compressed to $FILENAME"
