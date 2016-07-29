65ad9b5ebbfc083e7f2639067bf25227452ae4f0
Francois Joubert <fjoubert@ska.ac.za>
2016-07-29 09:59:20 +0200

    compress dist/ folder for upload

--- update.sh
+++ update.sh
@@ -14,3 +14,11 @@ npm install
 echo "## Performing gulp build ..."
 gulp build
 
+echo "## Compressing dist/ folder ..."
+GIT_REF=`git rev-parse --short HEAD`
+VERSION=`python -m json.tool dist/version.txt | grep -Po '\d\.\d\.\d'`
+DATE=`date +"%Y-%m-%d"`
+TIME=`date +"%H:%M"`
+FILENAME="katgui-$VERSION-$DATET$TTIME-$GIT_REF.tar.gz"
+tar -zcvf $FILENAME dist/
+echo "## KATGUI compressed to $FILENAME"
