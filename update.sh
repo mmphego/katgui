814d64c5601acec3a3deacfa9aa938f61998a7d7
Francois Joubert <fjoubert@ska.ac.za>
2016-08-29 20:59:35 +0200

    --allow-root for bower install because jenkinsfile runs as root

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ echo "## Updating bower and gulp ..."
 sudo -H npm update -g bower gulp
 
 echo "## Installing bower components ..."
-bower install
+bower install --allow-root
 
 echo "## Installing nodejs components ..."
 npm install
