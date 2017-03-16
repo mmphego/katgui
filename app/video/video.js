ed8c2f28c3da0e109b082efd31100ac6804912aa
Francois Joubert <fjoubert@ska.ac.za>
2017-03-16 12:39:05 +0200

    added dragability to the vds image feed for easier positioning

--- app/video/video.js
+++ app/video/video.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
     //ssh to the box, do
     //motion -n -c /var/kat/vds_motion.conf
@@ -6,18 +6,14 @@
         .controller('VideoCtrl', VideoCtrl);
 
     function VideoCtrl($scope, $rootScope, $http, $log, $interval, $mdDialog, ControlService, SensorsService,
-                       NotifyService, USER_ROLES, ConfigService, KatGuiUtil, $state) {
-
-        function urlBase() {
-           return $rootScope.portalUrl? $rootScope.portalUrl + '/katcontrol/vds/' + vm.vds_name : '';
-        }
+        NotifyService, USER_ROLES, ConfigService, KatGuiUtil, $state) {
 
         var vm = this;
         vm.imageSources = [];
         vm.imageSource = null;
 
         ConfigService.getSystemConfig()
-            .then(function (systemConfig) {
+            .then(function(systemConfig) {
 
                 if (!systemConfig.vds) {
                     $state.go('home');
@@ -28,7 +24,10 @@
                 var imageKeys = Object.keys(systemConfig.vds);
 
                 for (var i = 0; i < imageKeys.length; i++) {
-                    vm.imageSources.push({name: imageKeys[i], url: systemConfig.vds[imageKeys[i]]});
+                    vm.imageSources.push({
+                        name: imageKeys[i],
+                        url: systemConfig.vds[imageKeys[i]]
+                    });
                 }
 
                 if (!$scope.$$phase) {
@@ -39,10 +38,10 @@
         vm.sensorValues = {};
         vm.stepTimeValue = 1;
 
-        vm.toggleFloodLights = function () {
+        vm.toggleFloodLights = function() {
             vm.floodlightsOn(
-              vm.sensorValues[vm.vds_name + '_flood_lights_on'].value ? 'off' : 'on')
-                .then(function (result) {
+                    vm.sensorValues[vm.vds_name + '_flood_lights_on'].value ? 'off' : 'on')
+                .then(function(result) {
                     var splitMessage = result.data.result.split(' ');
                     var message = KatGuiUtil.sanitizeKATCPMessage(result.data.result);
                     if (splitMessage.length > 2 && splitMessage[1] !== 'ok') {
@@ -50,144 +49,144 @@
                     } else {
                         NotifyService.showSimpleToast(message);
                     }
-                }, function (error) {
+                }, function(error) {
                     NotifyService.showSimpleDialog('Error sending request', error);
                 });
         };
 
-        vm.SelectedSource = function (selected_item) {
+        vm.SelectedSource = function(selected_item) {
             vm.lastPreset = '';
-            var vds_names = {'core_south_camera':'cores',
-              'core_north_camera':'coren',
-              'high_elevation_camera':'high'};
+            var vds_names = {
+                'core_south_camera': 'cores',
+                'core_north_camera': 'coren',
+                'high_elevation_camera': 'high'
+            };
             vm.vds_name = 'vds' + vds_names[selected_item.name];
-        }
+        };
 
-        vm.floodlightsOn = function (onOff) {
+        vm.floodlightsOn = function(onOff) {
             return $http(createRequest('post', urlBase() + '/floodlights/' + onOff));
         };
 
-        vm.panLeft = function () {
+        vm.panLeft = function() {
             vm.vdsCommand('pan/left', vm.stepTimeValue);
         };
 
-        vm.panFarLeft = function () {
+        vm.panFarLeft = function() {
             vm.vdsCommand('pan', 'left');
         };
 
-        vm.panRight = function () {
+        vm.panRight = function() {
             vm.vdsCommand('pan/right', vm.stepTimeValue);
         };
 
-        vm.panFarRight = function () {
+        vm.panFarRight = function() {
             vm.vdsCommand('pan', 'right');
         };
 
-        vm.tiltUp = function () {
+        vm.tiltUp = function() {
             vm.vdsCommand('tilt/up', vm.stepTimeValue);
         };
 
-        vm.tiltFarUp = function () {
+        vm.tiltFarUp = function() {
             vm.vdsCommand('tilt', 'up');
         };
 
-        vm.tiltDown = function () {
+        vm.tiltDown = function() {
             vm.vdsCommand('tilt/down', vm.stepTimeValue);
         };
 
-        vm.tiltFarDown = function () {
+        vm.tiltFarDown = function() {
             vm.vdsCommand('tilt', 'down');
         };
 
-        vm.setPreset = function (event) {
+        vm.setPreset = function(event) {
             $mdDialog
                 .show({
-                    controller: function ($rootScope, $scope, $mdDialog) {
+                    controller: function($rootScope, $scope, $mdDialog) {
                         $scope.title = 'Set VDS Preset';
                         $scope.presetIDs = [];
                         for (var i = 0; i < 64; i++) {
                             $http(createRequest('post', urlBase() + '/presetset/'));
                             if (i < 10) {
                                 $scope.presetIDs.push('m00' + i);
-                            }
-                            else {
+                            } else {
                                 $scope.presetIDs.push('m0' + i);
                             }
                         }
-                        $scope.hide = function () {
+                        $scope.hide = function() {
                             $mdDialog.hide();
                         };
-                        $scope.setPreset = function (preset) {
+                        $scope.setPreset = function(preset) {
                             $http(createRequest('post', urlBase() + '/presetset/' + preset))
                                 .then(requestSuccess, requestError);
                         };
                     },
                     template: '<md-dialog style="padding: 0;" md-theme="{{$root.themePrimary}}">' +
-                    '   <div style="padding: 0; margin: 0; overflow: auto" layout="column">' +
-                    '       <md-toolbar class="md-primary" layout="row" layout-align="center center">' +
-                    '           <span flex style="margin: 8px;">{{::title}}</span>' +
-                    '       </md-toolbar>' +
-                    '       <div flex layout="row" layout-align="center center">' +
-                    '           <md-select ng-model="selectedPreset" style="margin:8px;"' +
-                    '               class="md-primary" placeholder="Select A Preset ID">' +
-                    '               <md-option ng-value="preset" ng-repeat="preset in presetIDs">{{preset}}</md-option>' +
-                    '           </md-select>' +
-                    '       </div>' +
-                    '       <div layout="row" layout-align="end" style="margin-top: 8px; margin-right: 8px; margin-bottom: 8px; min-height: 40px;">' +
-                    '           <md-button class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="hide()">Cancel</md-button>' +
-                    '           <md-button style="margin-left: 8px;" class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="setPreset(selectedPreset); hide()">Set Selected Preset</md-button>' +
-                    '       </div>' +
-                    '   </div>' +
-                    '</md-dialog>',
+                        '   <div style="padding: 0; margin: 0; overflow: auto" layout="column">' +
+                        '       <md-toolbar class="md-primary" layout="row" layout-align="center center">' +
+                        '           <span flex style="margin: 8px;">{{::title}}</span>' +
+                        '       </md-toolbar>' +
+                        '       <div flex layout="row" layout-align="center center">' +
+                        '           <md-select ng-model="selectedPreset" style="margin:8px;"' +
+                        '               class="md-primary" placeholder="Select A Preset ID">' +
+                        '               <md-option ng-value="preset" ng-repeat="preset in presetIDs">{{preset}}</md-option>' +
+                        '           </md-select>' +
+                        '       </div>' +
+                        '       <div layout="row" layout-align="end" style="margin-top: 8px; margin-right: 8px; margin-bottom: 8px; min-height: 40px;">' +
+                        '           <md-button class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="hide()">Cancel</md-button>' +
+                        '           <md-button style="margin-left: 8px;" class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="setPreset(selectedPreset); hide()">Set Selected Preset</md-button>' +
+                        '       </div>' +
+                        '   </div>' +
+                        '</md-dialog>',
                     targetEvent: event
                 });
         };
 
-        vm.goToPreset = function (event) {
+        vm.goToPreset = function(event) {
             $mdDialog
                 .show({
-                    controller: function ($rootScope, $scope, $mdDialog) {
+                    controller: function($rootScope, $scope, $mdDialog) {
                         $scope.title = 'Go To VDS Preset';
                         $scope.presetIDs = [];
                         for (var i = 0; i < 64; i++) {
                             if (i < 10) {
                                 $scope.presetIDs.push('m00' + i);
-                            }
-                            else {
+                            } else {
                                 $scope.presetIDs.push('m0' + i);
                             }
                         }
-                        $scope.hide = function () {
+                        $scope.hide = function() {
                             $mdDialog.hide();
                         };
-                        $scope.gotoPreset = function (preset) {
+                        $scope.gotoPreset = function(preset) {
                             vm.lastPreset = preset;
                             $http(createRequest('post', urlBase() + '/presetgoto/' + preset))
                                 .then(requestSuccess, requestError);
                         };
                     },
                     template: '<md-dialog style="padding: 0;" md-theme="{{$root.themePrimary}}">' +
-                    '   <div style="padding: 0; margin: 0; overflow: auto" layout="column">' +
-                    '       <md-toolbar class="md-primary" layout="row" layout-align="center center">' +
-                    '           <span flex style="margin: 8px;">{{::title}}</span>' +
-                    '       </md-toolbar>' +
-                    '       <div flex layout="row" layout-align="center center">' +
-                    '           <md-select ng-model="selectedPreset" style="margin:8px;"' +
-                    '               class="md-primary" placeholder="Select A Preset ID">' +
-                    '               <md-option ng-value="preset" ng-repeat="preset in presetIDs">{{preset}}</md-option>' +
-                    '           </md-select>' +
-                    '       </div>' +
-                    '       <div layout="row" layout-align="end" style="margin-top: 8px; margin-right: 8px; margin-bottom: 8px; min-height: 40px;">' +
-                    '           <md-button class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="hide()">Cancel</md-button>' +
-                    '           <md-button style="margin-left: 8px;" class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="gotoPreset(selectedPreset); hide()">Go To Selected Preset</md-button>' +
-                    '       </div>' +
-                    '   </div>' +
-                    '</md-dialog>',
+                        '   <div style="padding: 0; margin: 0; overflow: auto" layout="column">' +
+                        '       <md-toolbar class="md-primary" layout="row" layout-align="center center">' +
+                        '           <span flex style="margin: 8px;">{{::title}}</span>' +
+                        '       </md-toolbar>' +
+                        '       <div flex layout="row" layout-align="center center">' +
+                        '           <md-select ng-model="selectedPreset" style="margin:8px;"' +
+                        '               class="md-primary" placeholder="Select A Preset ID">' +
+                        '               <md-option ng-value="preset" ng-repeat="preset in presetIDs">{{preset}}</md-option>' +
+                        '           </md-select>' +
+                        '       </div>' +
+                        '       <div layout="row" layout-align="end" style="margin-top: 8px; margin-right: 8px; margin-bottom: 8px; min-height: 40px;">' +
+                        '           <md-button class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="hide()">Cancel</md-button>' +
+                        '           <md-button style="margin-left: 8px;" class="md-primary md-raised" md-theme="{{$root.themePrimaryButtons}}" ng-click="gotoPreset(selectedPreset); hide()">Go To Selected Preset</md-button>' +
+                        '       </div>' +
+                        '   </div>' +
+                        '</md-dialog>',
                     targetEvent: event
                 });
         };
 
-        vm.zoomChanged = function (zoomValue) {
+        vm.zoomChanged = function(zoomValue) {
             vm.zoom = zoomValue;
             if (vm.zoomInterval) {
                 $interval.cancel(vm.zoomInterval);
@@ -195,14 +194,14 @@
             vm.zoomInterval = $interval(vm.sendCurrentZoomCommand, 1000);
         };
 
-        vm.sendCurrentZoomCommand = function () {
+        vm.sendCurrentZoomCommand = function() {
             vm.vdsCommand('zoom/to', vm.zoom);
             if (vm.zoomInterval) {
                 $interval.cancel(vm.zoomInterval);
             }
         };
 
-        vm.focusChanged = function (focusValue) {
+        vm.focusChanged = function(focusValue) {
             vm.focus = focusValue;
             if (vm.focusInterval) {
                 $interval.cancel(vm.focusInterval);
@@ -210,19 +209,19 @@
             vm.focusInterval = $interval(vm.sendCurrentFocusCommand, 1000);
         };
 
-        vm.sendCurrentFocusCommand = function () {
+        vm.sendCurrentFocusCommand = function() {
             vm.vdsCommand('focus/to', vm.focus);
             if (vm.focusInterval) {
                 $interval.cancel(vm.focusInterval);
             }
         };
 
-        vm.stopVDS = function () {
+        vm.stopVDS = function() {
             $http(createRequest('post', urlBase() + '/stop'))
                 .then(requestSuccess, requestError);
         };
 
-        vm.vdsCommand = function (endpoint, args) {
+        vm.vdsCommand = function(endpoint, args) {
             $http(createRequest('post', urlBase() + '/' + endpoint + '/' + args))
                 .then(requestSuccess, requestError);
         };
@@ -235,9 +234,9 @@
             NotifyService.showSimpleDialog('Error sending VDS command.', result.data);
         }
 
-        vm.connectListeners = function () {
+        vm.connectListeners = function() {
             SensorsService.connectListener()
-                .then(function () {
+                .then(function() {
                     vm.initSensors();
                     if (vm.connectInterval) {
                         $interval.cancel(vm.connectInterval);
@@ -246,7 +245,7 @@
                             NotifyService.showSimpleToast('Reconnected :)');
                         }
                     }
-                }, function () {
+                }, function() {
                     $log.error('Could not establish sensor connection. Retrying every 10 seconds.');
                     if (!vm.connectInterval) {
                         vm.connectInterval = $interval(vm.connectListeners, 10000);
@@ -255,9 +254,9 @@
             vm.handleSocketTimeout();
         };
 
-        vm.handleSocketTimeout = function () {
+        vm.handleSocketTimeout = function() {
             SensorsService.getTimeoutPromise()
-                .then(function () {
+                .then(function() {
                     if (!vm.disconnectIssued) {
                         NotifyService.showSimpleToast('Connection timeout! Attempting to reconnect...');
                         if (!vm.connectInterval) {
@@ -268,7 +267,7 @@
                 });
         };
 
-        vm.initSensors = function () {
+        vm.initSensors = function() {
             SensorsService.setSensorStrategies('anc_vds_*', 'event-rate', 1, 360);
         };
 
@@ -286,7 +285,7 @@
         vm.unbindLoginSuccess = $rootScope.$on('loginSuccess', vm.afterInit);
         vm.afterInit();
 
-        var unbindUpdate = $rootScope.$on('sensorsServerUpdateMessage', function (event, sensor) {
+        var unbindUpdate = $rootScope.$on('sensorsServerUpdateMessage', function(event, sensor) {
             var sensorName = sensor.name.split(':')[1].replace('anc_', '');
             vm.sensorValues[sensorName] = sensor.value;
             if (sensorName === vm.vds_name + '_focus_position') {
@@ -296,7 +295,7 @@
             }
         });
 
-        $scope.$on('$destroy', function () {
+        $scope.$on('$destroy', function() {
             unbindUpdate();
             vm.disconnectIssued = true;
             SensorsService.disconnectListener();
@@ -304,6 +303,10 @@
                 vm.unbindLoginSuccess();
             }
         });
+
+        function urlBase() {
+            return $rootScope.portalUrl ? $rootScope.portalUrl + '/katcontrol/vds/' + vm.vds_name : '';
+        }
 
         function createRequest(method, url) {
             return {
