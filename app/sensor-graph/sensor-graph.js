f1168404744c4e7a0fd49f2ea88618a7baddcd62
Francois Joubert <fjoubert@ska.ac.za>
2018-02-20 11:45:28 +0200

    added resize buttons to the sensor graph search panel

--- app/sensor-graph/sensor-graph.js
+++ app/sensor-graph/sensor-graph.js
@@ -36,6 +36,7 @@
         vm.plotUsingValueTimestamp = $localStorage['plotUsingValueTimestamp']? true: false;
         vm.includeValueTimestamp = $localStorage['includeValueTimestamp']? true: false;
         vm.useUnixTimestamps = $localStorage['useUnixTimestamps']? true: false;
+        vm.searchWidth = $localStorage['sensorGraphSearchWidth']? $localStorage['sensorGraphSearchWidth'] : 366;
 
         vm.includeValueTimestampChanged = function () {
             $localStorage['includeValueTimestamp'] = vm.includeValueTimestamp;
@@ -418,6 +419,21 @@
                 vm.searchText = oldSearchText;
             }, 0);
             event.stopPropagation();
+        };
+
+        vm.increaseSearchWidth = function () {
+            vm.searchWidth += 150;
+            $localStorage['sensorGraphSearchWidth'] = vm.searchWidth;
+        };
+
+        vm.decreaseSearchWidth = function () {
+            vm.searchWidth -= 150;
+            $localStorage['sensorGraphSearchWidth'] = vm.searchWidth;
+        };
+
+        vm.resetSearchWidth = function (newSize) {
+            vm.searchWidth = newSize;
+            $localStorage['sensorGraphSearchWidth'] = vm.searchWidth;
         };
 
         vm.autoCompleteKeyPressed = function (event) {
