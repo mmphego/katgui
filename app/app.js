0173e2332d1f1d131d9ff791519c3365a68c3c29
ofaletse <ofaletse@gmail.com>
2018-03-14 08:34:50 +0200

    sort the config health list that appear on the left side navigator

--- app/app.js
+++ app/app.js
@@ -211,14 +211,9 @@
             ConfigService.getConfigHealthViews().then(
                 function(result) {
                     $rootScope.configHealthViews = [];
-                    var the_keys = [];
                     _.each(result.data, function(value, key, obj) {
-                        the_keys.push(key);
+                        $rootScope.configHealthViews.push(key);
                     });
-                    the_keys.sort();
-                    for (var key = 0; key < the_keys.length; key++) {
-                        $rootScope.configHealthViews.push(the_keys[key]);
-                    };
                 },
                 function(error) {
                     $log.error(error);
