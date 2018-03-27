21f0d56785f97828eec1b908d6f08e1fbfda6a8c
ofaletse <ofaletse@gmail.com>
2018-03-27 13:03:52 +0200

    add a comment that clarify where the constants come from

--- app/video/video.js
+++ app/video/video.js
@@ -15,6 +15,11 @@
         vm.sensorValues = {};
         vm.stepTimeValue = 1;
         vm.sensorsRegex = '^anc_vds';
+        /*
+            Note the following constants(vm.focusParams and vm.zoomParams) map the
+            with the params(min, max) for sensors vds-focus and vds-zoom respectively
+            vds translator handle all the scaling.
+        */
         vm.focusParams = {
           'min': 0,
           'max': 900
