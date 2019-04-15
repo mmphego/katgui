c14fc056246200b70588de3f36cd6a15024e52e3
Bulelani Xaia <bxaia@ska.ac.za>
2019-04-15 17:08:30 +0200

    Addressing PR messages

--- app/scheduler/scheduler-home.js
+++ app/scheduler/scheduler-home.js
@@ -1043,6 +1043,7 @@
                             systemConfig['katconn:resources'].single_ctl,
                             '^(' + systemConfig['katconn:resources'].single_ctl.replace(/,/g, '|') + ').state$', true)
                         .then(handleListSensorsResult, handleListSensorsError);
+                    // systemConfig['katconn:arrays'].ants example: m011,m022,s0003
                     MonitorService.listSensorsHttp(
                         systemConfig['katconn:arrays'].ants,
                         '^(' + systemConfig['katconn:arrays'].ants.replace(/,/g, '|') + ').ridx_position$', true)
