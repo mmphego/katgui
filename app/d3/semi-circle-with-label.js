39a776f2baac5fbb076bfb4fbeb02d40893addb7
Xinyu Wu <xwu@ska.ac.za>
2019-08-07 14:36:58 +0200

    Working static display.

--- app/d3/semi-circle-with-label.js
+++ app/d3/semi-circle-with-label.js
@@ -1,66 +1,88 @@
 angular.module('katGui.d3')
 
-.directive('semiCircleWithLabel', function($rootScope, $timeout, d3Util) {
+.directive('semiCircleWithLabel', function($window, $rootScope, $timeout, d3Util) {
     return {
+        scope: {
+            vm: '=',
+            type: '@type'
+        },
         link: function(scope, element) {
-            var skarabTexts = null;
-
-            var margin = {
-                top: 0,
-                right: 10,
-                left: 10,
-                bottom: 0
-            };
-
-            var width, height, radius;
             var border = 2;
+            var heightAdjust = 3;
 
-            var the_svg = d3.select(element[0]).append("svg")
-                    .attr("width", '100%')
-                    .attr("height", '50%');
+            var the_svg = d3.select(element[0]).append('svg')
+                            .attr('width', '100%')
+                            .attr('height', '100%');
 
-            scope.vm.svgList.push(the_svg);
+            var statusObj = {'svg': the_svg,
+                            'type': scope.type}
 
-            redrawStatus = function (svg) {
-                width = svg[0][0].clientWidth - margin.left - margin.right;
-                height = svg[0][0].clientHeight - margin.top - margin.bottom;
+            scope.vm.svgList.push(statusObj);
 
-                radius = width;
-                if (height < width)
+            redrawStatus = function (statusObj) {
+                var width, height, radius;
+                var svg = statusObj['svg'];
+                var type = statusObj['type'];
+
+                svg.select('path').remove();
+                width = svg[0][0].clientWidth;
+                height = svg[0][0].clientHeight - heightAdjust;
+
+                radius = width/2;
+                if (height < width/2)
                   radius = height;
 
                 var arc = d3.svg.arc();
-                var halfcircle = function(x,y,rad) {
+
+                var tophalfcircle = function(x,y,rad) {
                 	return svg.append('path')
-                	.attr('transform', 'translate('+[x,y]+')')
+                        	.attr('transform', 'translate('+[x,y]+')')
+                          .attr('d', arc({
+                          		innerRadius: 0,
+                          		outerRadius: rad,
+                          		startAngle: Math.PI*0.5,
+                          		endAngle: -Math.PI*0.5
+                        	}))
+                          .style('fill', '#4CAF50');
+                }
+                var bottomhalfcircle = function(x,y,rad) {
+                	return svg.append('path')
+                  .attr('transform', 'translate('+[x,y]+') rotate(180 0 0) ')
                   .attr('d', arc({
                   		innerRadius: 0,
                   		outerRadius: rad,
                   		startAngle: -Math.PI*0.5,
                   		endAngle: Math.PI*0.5
-                	}));
+                	}))
+                  .style('fill', '#FFC107');
                 }
-                halfcircle(width/2, height, radius);
+                if (type == 'top') {
+                  tophalfcircle(width/2, height, radius);
+                  svg.select('text').remove();
+                  svg.append('text').text('5').attr('x', width/2)
+                     .attr('y', height - 20);
+                } else {
+                  bottomhalfcircle(width/2, 0, radius);
+                  svg.select('text').remove();
+                  svg.append('text').text('5').attr('x', width/2)
+                      .attr('y', 60);
+                }
+
             };
 
-            var unbindResize = scope.$watch(function() {
-                return element[0].clientHeight + ', ' + element[0].clientWidth;
-            }, function(newVal, oldVal) {
-                if (scope.resizeTimeout) {
-                    $timeout.cancel(scope.resizeTimeout);
-                    scope.resizeTimeout = null;
-                }
-                //allow for some time for the dom elements to complete resizing
-                scope.resizeTimeout = $timeout(function() {
-                  for (var i=0; i<scope.vm.svgList.length; i++)
-                    redrawStatus(scope.vm.svgList[i]);
-                }, 1000);
+            angular.element($window).bind('resize', function(){
+              for (var i=0; i<scope.vm.svgList.length; i++)
+                redrawStatus(scope.vm.svgList[i]);
             });
+
+            //allow for some time for the dom elements to complete resizing
+            $timeout(function() {
+                redrawStatus(statusObj);
+            }, 1000);
 
             scope.$on('$destroy', function() {
               unbindResize();
             });
-
 
             scope.vm.updateStatus = function(obj) {
               //
