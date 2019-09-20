4429b339edffc7470b2884f9b24854a9e3c4a1c9
Buntu Ngcebetsha <bngcebetsha@ska.ac.za>
2019-09-20 20:11:38 +0200

    Merge pull request #490 from ska-sa/user/bngcebetsha/MT-979/assign-all-usable-receptors-at-once
    Button to assign all usable receptors at once

--- app/scheduler/subarray-resources/subarray-resources.js
+++ app/scheduler/subarray-resources/subarray-resources.js
@@ -49,6 +49,28 @@
             ObsSchedService.assignResourcesToSubarray(vm.subarray.id, resourceName);
         };
 
+        vm.assignAllReceptors = function () {
+            var allReceptors = ConfigService.systemConfig["antenna_labels"]["ALL"].split(',');
+            var freeResources = [];
+            for (var j=0; j<vm.poolResourcesFree.length; j++)
+                freeResources.push(vm.poolResourcesFree[j].name);
+
+            for (var i=0; i<allReceptors.length; i++) {
+                var receptorName = allReceptors[i];
+                var isReceptorInMaintenance = $scope.parent.vm.isResourceInMaintenance(receptorName);
+                var isReceptorFaulty = $scope.parent.vm.isResourceFaulty(receptorName);
+                var subarrayInMaintenance = $scope.parent.vm.sensorValues[vm.subarray.name + '_maintenance'].value;
+                if (!freeResources.includes(receptorName))
+                    continue;
+
+                if (!subarrayInMaintenance) {
+                    if (isReceptorInMaintenance || isReceptorFaulty)
+                        continue;
+                }
+
+                ObsSchedService.assignResourcesToSubarray(vm.subarray.id, receptorName);
+            }
+        };
 
         vm.resourceAllowedInSubarray = function (resourceName) {
             var genericResources = [];
