f294be2ee2ba06f00fadfe71aafbc4cd485da9d0
Mpho Mphego <mpho112@gmail.com>
2019-11-23 23:40:09 +0200

    docstring fixes

--- tour/utils.py
+++ tour/utils.py
@@ -6,11 +6,13 @@ from gtts import gTTS
 
 
 class TexttoSpeech:
-    def __init__(self, text: str, filename: str, assets_dir: str = "assets") -> None:
+    def __init__(
+        self, text: str, filename: str, assets_dir: str = "assets", ext: str = "mp3"
+    ) -> None:
         self.text = text
         if not Path(assets_dir).is_dir():
             Path(assets_dir).mkdir()
-        self.filename = Path(f"{assets_dir}/{filename}.mp3").absolute()
+        self.filename = Path(f"{assets_dir}/{filename}.{ext}").absolute()
 
     @staticmethod
     def which(program: str) -> str:
@@ -21,10 +23,12 @@ class TexttoSpeech:
             return subprocess.check_output(["which", program]).strip().decode()
 
     def text_to_speech(self) -> gTTS:
+        """Generate speech from text using Google TTS API."""
         tts = gTTS(self.text)
         return tts
 
     def save_to_mp3(self) -> None:
+        """Save generated TTS as mp3 files."""
         tts = self.text_to_speech()
         tts.save(self.filename)
 
@@ -33,21 +37,18 @@ class TexttoSpeech:
         if not self.filename.is_file():
             self.save_to_mp3()
 
-        msg = (
-            "Ensure that mpg123/vlc is installed in your system\n"
-            "Run 'sudo apt install --install-recommends mpg123' or\n"
-            "Run 'sudo apt install --install-recommends vlc vlc-bin' "
-        )
-        if not play_with:
-            raise RuntimeError(msg)
-
         FNULL = open(subprocess.os.devnull, "wb")
         play_with = (
             self.which(play_with)
             if self.which(play_with) is not None
             else self.which("mpg123")
         )
-        assert play_with, f"No defined player.\n{msg}"
+        err_msg = (
+            "No Player installed, ensure that either 'mpg123' or 'vlc' is installed "
+            "in your system.\n"
+            "Run: 'sudo apt install --install-recommends mpg123 vlc vlc-bin'"
+        )
+        assert play_with, err_msg
 
         if "cvlc" in play_with:
             player_cmd = f"{play_with} --play-and-exit --no-loop"
