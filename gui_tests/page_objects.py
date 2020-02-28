9085043016719b150bcca8c276a147148f17f4c9
Mpho Mphego <mpho112@gmail.com>
2020-02-28 16:07:18 +0200

    Added gui xpath;

---
+++ gui_tests/page_objects.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class SubArray:
+    html = "html"
+    username = "#input_1"
+    password = "#input_2"
+    select_user = "//md-select-value[@id='select_value_label_0']/span"
+    expert_user = "//md-option[@id='select_option_6']/div"
+    submit = "//button[@type='submit']"
+    menu_bar = "//md-toolbar[@id='main-top-toolbar']/div/button[4]/span"
+    notification_checkbox = "//md-checkbox/div[2]"
+    alarm_checkbox = "//md-item-content[3]/md-checkbox/div[2]"
+    back = "//md-backdrop"
+    subarray = "(//button[@type='button'])[66]"
+    free = "//md-content[@id='ui-view-container-div']/div/div[3]/div/div/div/div/div[2]/md-toolbar/span[5]"
+    select_mode = "//md-content[@id='ui-view-container-div']/div/div[3]/div/div/div/div/div[2]/md-toolbar/div/div/md-menu[2]/span/b"
+    button = "(//button[@type='button'])[112]"
+    cbf = "//md-content[@id='ui-view-container-div']/div/div[3]/div/div/div[3]/div/div/div[2]/span"
+    antennas = "//md-content[@id='ui-view-container-div']/div/div[3]/div/div/div[3]/div/div/div[10]/span"
+    sdp = "//md-content[@id='ui-view-container-div']/div/div[3]/div/div/div[3]/div/div/div[79]/span"
+    initialize = "//md-content[@id='ui-view-container-div']/div/div[3]/div/div/div/div/div[2]/md-toolbar/span[4]"
