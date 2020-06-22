9b5faa86b419fc8e8dbb22c6da335453be0023d9
Buntu Ngcebetsha <bngcebetsha@ska.ac.za>
2020-06-22 18:02:55 +0200

    need to test with devbox

--- app/services/userlog-service.js
+++ app/services/userlog-service.js
@@ -396,6 +396,7 @@
                         $scope.openedWithoutEndTime = $scope.end_time !== null && $scope.end_time.length > 0;
                         $scope.chipHasBeenAdded = false;
                         $scope.focusTarget = focusTarget? focusTarget: 'userlogDialogStartTimeElement';
+                        $scope.internalModification = log.modified !== null;
 
                         $timeout(function () {
                             var parentElement = document.querySelector('#' + $scope.focusTarget);
